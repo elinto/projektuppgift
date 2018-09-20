0ccde80a44c1bd2da1198b69ab866185cd4b78ec
elinto <elin_torebrand94@hotmail.com>
2018-09-20 12:12:22 +0200

    bildspel

--- menu.js
+++ menu.js
@@ -1,9 +1,39 @@
-$(document).ready(function(){
-    $(".menuMobileIcon").click(function(){
+$(document).ready(function () {
+    $(".menuMobileIcon").click(function () {
         $(".menuContainer").show(200);
     })
 
-    $(".closeMenu").click(function(){
+    $(".closeMenu").click(function () {
         $(".menuContainer").hide(200);
     })
+
 });
+
+var bildSpel = [
+    "bilder/musik.jpg",
+    "bilder/bubbla.jpg",
+    "bilder/yay.jpg"
+];
+
+var currentIndex = 0;
+
+function visaBild() {
+    var img = document.getElementById("bildSpel");
+    img.setAttribute("src", bildSpel[currentIndex]);
+    currentIndex++;
+    if (currentIndex === bildSpel.length) {
+        currentIndex = 0;
+
+    }
+}
+function startaBildspel() {
+    setInterval(visaBild, 2000);
+
+}
+
+document.getElementsByClassName("pauseK").addEventListener("click", myFunction);
+function myFunction(){
+
+}
+
+
