0ec39b5b47529ad52897e83d7d206720dd3a44d6
elinto <elin_torebrand94@hotmail.com>
2018-09-20 12:52:03 +0200

    uppdaterad

--- menu.js
+++ menu.js
@@ -1,55 +1,60 @@
 $(document).ready(function () {
     $(".menuMobileIcon").click(function () {
         $(".menuContainer").show(200);
-    })
+    });
 
     $(".closeMenu").click(function () {
         $(".menuContainer").hide(200);
-    })
+    });
+    var bildSpel = [
+        "bilder/musik.jpg",
+        "bilder/bubbla.jpg",
+        "bilder/yay.jpg"
+    ];
+
+    var currentIndex = 0;
+
+    function visaBild() {
+        var img = document.getElementById("bildSpel");
+        img.setAttribute("src", bildSpel[currentIndex]);
+        currentIndex++;
+        if (currentIndex === bildSpel.length) {
+            currentIndex = 0;
+
+        }
+    }
+    var bildspelIntervalId = null;
+    startaBildspel();
+    function startaBildspel() {
+        visaBild();
+        bildspelIntervalId = setInterval(visaBild, 2000);
+    }
+
+    function stopBildspel() {
+        clearInterval(bildspelIntervalId);
+        bildspelIntervalId = null;
+    }
+
+    function toggleBildspel() {
+        var toggleKnapp = document.getElementById("pauseKnapp");
+        if (bildspelIntervalId) {
+            stopBildspel();
+            toggleKnapp.setAttribute("src", "bilder/play.png");
+            //$("#pauseKnapp").attr("src", "bilder/play.png");
+        } else {
+            startaBildspel();
+            toggleKnapp.setAttribute("src", "bilder/pause.png");
+        }
+    }
+
+    document.getElementById("pauseKnapp").addEventListener("click", toggleBildspel);
+    //$("#pauseKnapp").click(function () {
+    //    toggleBildspel();
+    //});
 
 });
 
-var bildSpel = [
-    "bilder/musik.jpg",
-    "bilder/bubbla.jpg",
-    "bilder/yay.jpg"
-];
 
-var currentIndex = 0;
-
-function visaBild() {
-    var img = document.getElementById("bildSpel");
-    img.setAttribute("src", bildSpel[currentIndex]);
-    currentIndex++;
-    if (currentIndex === bildSpel.length) {
-        currentIndex = 0;
-
-    }
-}
-var bildspelIntervalId = null;
-
-function startaBildspel() {
-    visaBild();
-    bildspelIntervalId = setInterval(visaBild, 2000);
-}
-
-function stopBildspel(){
-    clearInterval(bildspelIntervalId);
-    bildspelIntervalId = null;
-}
-
-function toggleBildspel(){
-    var toggleKnapp = document.getElementById("pauseKnapp");
-    if(bildspelIntervalId){
-        stopBildspel();
-        toggleKnapp.setAttribute("src", "bilder/play.png");
-    } else {
-        startaBildspel();
-        toggleKnapp.setAttribute("src", "bilder/pause.png");
-    }
-}
-
-document.getElementById("pauseKnapp").addEventListener("click", toggleBildspel);
 
 
 
